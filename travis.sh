57392663bb43b2d4ac4a03d73f6eecc9904d5882
Shailesh <sbirari@netflix.com>
2015-12-17 18:48:01 -0800

    Enable functional test in travis

--- travis.sh
+++ travis.sh
@@ -4,7 +4,26 @@
 #date   : 2015-10-20
 
 
+if [ -n "$TRAVIS" ]; then
+    #sudo apt-get install socat
+
+    #python libs
+    sudo pip install redis
+    sudo pip install git+https://github.com/andymccurdy/redis-py.git@2.10.3
+    #sudo pip install git+https://github.com/idning/python-memcached.git#egg=memcache
+fi
+
 #build Dynomite
 CFLAGS="-ggdb3 -O0" autoreconf -fvi && ./configure --enable-debug=log && make 
+
+mkdir test/_binaries
+ln -s `pwd`/src/nutcracker  test/_binaries/
+cp `which redis-server` test/_binaries/
+cp `which redis-cli` test/_binaries/
+cp `which memcached` test/_binaries/
+
+cd test
+
+./func_test.py
 
 
